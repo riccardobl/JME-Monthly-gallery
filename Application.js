a54602ab905e62e98eab02fd331a643b4ecd7aff
aegroto <dott.micronesia@gmail.com>
2015-12-17 20:38:22 +0100

    New smart effects

--- Application.js
+++ Application.js
@@ -190,7 +190,6 @@ function refreshView(){
     
     var l=GALLERY_ELEMENTS.length;
     l+=Settings.COLUMNS-(l%Settings.COLUMNS); // Assures that the length of the gallery is a multiple of Settings.COLUMNS
-    
     for(var i=0;i<l;i++){
         var post=$("<div></div>");
         post.addClass("post");
@@ -254,6 +253,34 @@ function refreshView(){
 //        post.appendTo($("#container"));
 //    }
     loading(false);
+}
+
+function setAutoHide(bind_to,elements){
+     new Waypoint.Inview({
+         element: bind_to,
+         enter: (function(direction) {
+             for(var i=0;i<this.length;i++){
+                 var el=this[i];
+                 if(!el)continue;
+                 var old_v=el.attr("old-display");
+                 if(old_v)el.css("display",old_v);
+             }
+
+         }).bind(elements),
+         exited: (function(direction) {
+             for(var i=0;i<this.length;i++){
+                var el=this[i];
+                if(!el)continue;
+                var old_v=el.css("display");
+                if(old_v){
+                    if(old_v==='none')continue;
+                    el.attr("old-display",old_v);
+                }
+                el.css("display","none");
+             }
+            
+         }).bind(elements)
+    });
 }
 
 
